4e326197bf1c0fbeb209f9a9674a1f8cceb84adf
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/MailReminderPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/MailReminderPlugin/cron.pl
+++ lib/TWiki/Plugins/MailReminderPlugin/cron.pl
@@ -13,52 +13,65 @@ use DBI;
 
 $cur_dir = $0;
 $cur_dir =~ s/\/[^\/]*$//;
-$config_file = $cur_dir."/config_file";
-open (CONFIG, $config_file);	# read the configuration file.
+$config_file = $cur_dir . "/config_file";
+open( CONFIG, $config_file );    # read the configuration file.
 
-while (<CONFIG>)
-{
-	chomp;
-	s/#.*//;
-	s/^\s+//;
-	s/\s+$//;
-	next unless length;
-	my ($var, $value) = split (/\s*=\s*/, $_, 2);
-	$$var = $value;		# initialise configuration variables.
+while (<CONFIG>) {
+    chomp;
+    s/#.*//;
+    s/^\s+//;
+    s/\s+$//;
+    next unless length;
+    my ( $var, $value ) = split( /\s*=\s*/, $_, 2 );
+    $$var = $value;              # initialise configuration variables.
 }
-close (CONFIG);
+close(CONFIG);
 
-$dbh = Mysql->connect($host,$database,$user,$passwd);	#connect to database.
+$dbh = Mysql->connect( $host, $database, $user, $passwd ); #connect to database.
 
-($day, $month, $year) = (localtime)[3,4,5];		# take todays date.
+( $day, $month, $year ) = (localtime)[ 3, 4, 5 ];          # take todays date.
 
-$year = $year+1900;
-$month = $month+1;
+$year  = $year + 1900;
+$month = $month + 1;
 
-$query = "select name, e_date, event, email_id from caltab where r_date = \'".$year."-".$month."-".$day."\'";	# form query to get the events.
+$query =
+    "select name, e_date, event, email_id from caltab where r_date = \'" 
+  . $year . "-"
+  . $month . "-"
+  . $day
+  . "\'";    # form query to get the events.
 
-$sth = $dbh->query($query);	# execute query.
+$sth = $dbh->query($query);    # execute query.
 
-while (@row = $sth->fetchrow)
-{
-	($name, $e_date, $event, $email_id) = @row;
-	send_mail ($name, $e_date, $event, $email_id);	# for each event row send the mail.
+while ( @row = $sth->fetchrow ) {
+    ( $name, $e_date, $event, $email_id ) = @row;
+    send_mail( $name, $e_date, $event, $email_id )
+      ;                        # for each event row send the mail.
 }
 
-$query = "delete from caltab where r_date = \'".$year."-".$month."-".$day."\'";	
+$query =
+    "delete from caltab where r_date = \'" 
+  . $year . "-" 
+  . $month . "-" 
+  . $day . "\'";
+
 # form query to delete the events.
-$sth = $dbh->query($query);	# execute query.
+$sth = $dbh->query($query);    # execute query.
 $sth->finish();
 
-
-sub send_mail
-{
-	$mail{From} = $email_from;
-	$mail{To}   = $_[3];
-	$server = $mailserver;
-	use Mail::Sendmail;
-	$mail{Smtp} = $server;
-	$mail{Subject} = "Event Reminder.";
-	$mail{Message} = "Dear ".$_[0].",\nThis is to remind you that on date : ".$_[1]." there is event : ".$_[2]."\n\n"."Regards,\n-Event Reminder Team." ;
-	sendmail %mail	
+sub send_mail {
+    $mail{From} = $email_from;
+    $mail{To}   = $_[3];
+    $server     = $mailserver;
+    use Mail::Sendmail;
+    $mail{Smtp}    = $server;
+    $mail{Subject} = "Event Reminder.";
+    $mail{Message} = "Dear "
+      . $_[0]
+      . ",\nThis is to remind you that on date : "
+      . $_[1]
+      . " there is event : "
+      . $_[2] . "\n\n"
+      . "Regards,\n-Event Reminder Team.";
+    sendmail %mail;
 }
